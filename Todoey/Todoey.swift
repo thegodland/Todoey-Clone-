e4d57399102a69b4193ebdd9db1e05ab5c47a6a2
刘祥 <xiangliu@s-macbook-pro-3.local>
2018-07-12 17:29:53 -0400

    finished the datasource and delegate methods

---
+++ Todoey/Todoey.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by 刘祥 on 7/11/18.
+//  Copyright © 2018 Orange Arrow. All rights reserved.
+//
+
+import UIKit
+
+class Todoey: UITableViewController {
+    
+    let itemArray = []
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "TodoeyCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+
+
+
+}
+
